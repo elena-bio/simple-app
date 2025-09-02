9e03aced7ceeeedd4e91242879171f8e657c8034
Elena <elham.hosseiny.ha@gmail.com>
2025-09-02 11:28:44 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Sum:", add_numbers(5, 3))
+    print("Difference:", subtract_numbers(5, 3))
